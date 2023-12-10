b6f6b07f2b79596030d1e1f5527c928f8aae6fc1
Ishu2654 <114808856+Ishu2654@users.noreply.github.com>
2023-12-10 19:29:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather Forecast',
+      title: 'Sky Vision',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
